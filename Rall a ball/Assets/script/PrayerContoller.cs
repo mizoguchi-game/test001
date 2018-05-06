ae07b2dfeb8376c93679fca56fb7ef5f6cb78992
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-05-07 00:28:49 +0900

    Rall a ball 完成

---
+++ Rall a ball/Assets/script/PrayerContoller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrayerContoller : MonoBehaviour {
+
+    public float speed = 20;
+    public Text scoreText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetCountText();
+        winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+
+            score = score + 1;
+
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = "Count: " + score.ToString();
+
+        if (score >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
