826e963493844ab3accadca8ae04ee659a9bac96
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-05-08 00:39:50 +0900

    block崩しボール動作開始まで

---
+++ blockbreak/Assets/Script/BallScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    public float Speed = 15.0f;
+
+    private Vector3 vector3 = new Vector3(0, 0, 0);
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("Jump") && transform.position == vector3)
+        {
+            GetComponent<Rigidbody>().AddForce((transform.forward + transform.right) * Speed, ForceMode.VelocityChange);
+        }
+	}
+}
