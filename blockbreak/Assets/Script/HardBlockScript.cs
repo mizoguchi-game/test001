88bafa2d48eb40fc94d8cb5f48f6d3147b238e5a
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-05-09 01:10:15 +0900

    貫通block・hardblock追加

---
+++ blockbreak/Assets/Script/HardBlockScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HardBlockScript : MonoBehaviour {
+
+    public int hp = 2;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        hp--;
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ blockbreak/Assets/Script/PenetrateBlockScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenetrateBlockScript : MonoBehaviour {
+
+    void Start()
+    { 
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter ()
+    {
+        Destroy(gameObject);
+    }
+}
