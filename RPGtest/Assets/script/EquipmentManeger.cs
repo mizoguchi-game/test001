89cfe738c2f40005c8fdb78a5c6f204d38119125
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-05-13 23:20:26 +0900

    武器持ち替えscript作成開始

---
+++ RPGtest/Assets/script/EquipmentManeger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManeger : MonoBehaviour {
+
+    public string WeaponPoint;
+
+    private GameObject m_WeaponPoint;
+    private GameObject m_Weapon = null;
+
+    // Use this for initialization
+    void Start () {
+        var children = GetComponentsInChildren<Transform>(true);
+        foreach(var transfom in children)
+        {
+            m_WeaponPoint = transfom.gameObject;
+        }
+	}
+	
+	public void EquipWepon(string name)
+    {
+        //すでに武器が作成されていた場合削除する
+        if(m_Weapon != null)
+        {
+            Destroy(m_Weapon);
+            m_Weapon = null;
+            Resources.UnloadUnusedAssets();
+        }
+
+        //Prefabをインスタンス化
+        m_Weapon = Instantiate(Resources.Load(name), m_WeaponPoint.transform.position, m_WeaponPoint.transform.rotation) as GameObject;
+
+        //見えない武器の子として登録
+        m_Weapon.transform.parent = m_WeaponPoint.transform;
+    }
+}
---
+++ RPGtest/Assets/script/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject charcter;
+
+    public enum waponTypes : int
+    {
+        SWORD,
+        WAND,
+        NUM,
+    }
+
+    private int m_weaponNo = 0;
+
+    private string[] m_weaponsPath = new string[]
+    {
+        "sword","wand"
+    };
+
+    private float m_kayInterval = 0f;
+
+    private EquipmentManeger m_characterEquipmentManager;
+
+    void Start()
+    {
+        m_characterEquipmentManager = charcter.GetComponent<EquipmentManeger>();
+    }
+
+    void Update()
+    {
+        if(Time.time - m_kayInterval > 0.5f)
+        {
+           if(Input.GetKey(KeyCode.RightArrow) == true)
+            {
+                m_kayInterval = Time.time;
+                NextWeapon();
+            }else if (Input.GetKey(KeyCode.LeftArrow) == true)
+            {
+                m_kayInterval = Time.time;
+                PrevWeapon();
+            }
+
+        }
+    }
+}
