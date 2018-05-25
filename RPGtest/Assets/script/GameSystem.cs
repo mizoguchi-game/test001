cf4a21aac9cd41919e23ede7ff58f2755b0dc23d
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-05-26 00:31:08 +0900

    ゲーム開始画面・遷移作成

---
+++ RPGtest/Assets/script/GameSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour {
+
+	public void GameStart()
+    {
+        SceneManager.LoadScene("test");
+    }
+
+    public void GameEnd()
+    {
+
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
