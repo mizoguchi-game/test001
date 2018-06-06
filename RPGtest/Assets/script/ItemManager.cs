c77f7d9cf99e948a3cc7c7247cf9f74032af4e0e
mizoguchi-game <mizoguchi.game@gmail.com>
2018-06-06 18:41:31 +0900

    巡回範囲の調整OK。アイテム作成（動かない）。一時停止画面作成開始

---
+++ RPGtest/Assets/script/ItemManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RotaryHeart.Lib.SerializableDictionary;
+
+[SerializeField]
+public class ItemDictionary : SerializableDictionaryBase<ItemManager.Item, int> { }
+
+public class ItemManager : MonoBehaviour {
+
+    public enum Item
+    {
+        Sword,
+        Gun,
+        Staff
+    }
+
+    [SerializeField]
+    public ItemDictionary itemDictionary;
+
+    public IDictionary<Item,int> ItemDictionary
+    {
+        get { return itemDictionary; }
+        set { itemDictionary.CopyFrom(value); }
+    }
+
+	// Use this for initialization
+	void Start () {
+        ItemDictionary[Item.Sword] = 0;
+        ItemDictionary[Item.Staff] = 1;
+
+        foreach(var item in ItemDictionary)
+        {
+            Debug.Log(item.Key + ":" + GetNum(item.Key));
+        }
+		
+	}
+
+    //アイテムをどれだけ保持しているのかの数を返す
+    public int GetNum(Item key)
+    {
+        return itemDictionary[key];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
