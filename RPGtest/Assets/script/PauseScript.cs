2df826663423a92bf8f4781bf315b41d9b1b76bd
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-06-06 23:58:43 +0900

    pause画面作成。次回アイテム画面作成から。

---
+++ RPGtest/Assets/script/PauseScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+    //ポーズにした時に表示するUI
+    [SerializeField]
+    private GameObject pauseUI;
+
+    //ポーズUIのインスタンス
+    private GameObject instancePauseUI;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("q"))
+        {
+            if (instancePauseUI == null)
+            {
+                instancePauseUI = GameObject.Instantiate(pauseUI) as GameObject;
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Destroy(instancePauseUI);
+                Time.timeScale = 1f;
+            }
+        }
+	}
+}
