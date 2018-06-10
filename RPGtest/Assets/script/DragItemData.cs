519303758f04dd24788b49b312dfcf3a14547a00
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-06-11 00:30:27 +0900

    持ち物画面で装備スロットに装備品を入れる機能作成開始。DragItemDataまで作成完了

---
+++ RPGtest/Assets/script/DragItemData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragItemData : MonoBehaviour {
+
+    //ドラックしているアイテム
+    private ItemData dragItemData;
+
+    //ドラッグするアイテムデータをセット
+	public void SetDragItem(ItemData dragItemData)
+    {
+        this.dragItemData = dragItemData;
+        transform.GetComponent<Image>().sprite = dragItemData.GetItemSprite();
+    }
+	
+    //ドラッグしているアイテムデータの取得
+    public ItemData GetItem()
+    {
+        return dragItemData;
+    }
+
+    //ドラッグしているアイテムの削除
+    public void DeleteDragItem()
+    {
+        dragItemData = null;
+    }
+
+    //ドラックアイテムが設定されている場合はマウスを追従
+	void Update () {
+        if (dragItemData != null)
+        {
+            transform.position = Input.mousePosition;
+        }
+	}
+}
--- RPGtest/Assets/script/PauseScript.cs
+++ RPGtest/Assets/script/PauseScript.cs
@@ -16,6 +16,7 @@ public class PauseScript : MonoBehaviour {
 
     private RectTransform title;
     private RectTransform main;
+    private RectTransform equip;
     private RectTransform information;
 
     private int width;
@@ -27,13 +28,15 @@ public class PauseScript : MonoBehaviour {
     {
         title = PropertyUI.transform.GetChild(0).Find("Title").GetComponent<RectTransform>();//11%
         main = PropertyUI.transform.GetChild(0).Find("main").GetComponent<RectTransform>();//68%
+        equip = PropertyUI.transform.GetChild(0).Find("Equip").GetComponent<RectTransform>();//68%
         information = PropertyUI.transform.GetChild(0).Find("Information").GetComponent<RectTransform>();//20%
 
         width = Screen.width;
         height = Screen.height;
 
         title.sizeDelta = new Vector2(width,(float)(height * 0.11));
-        main.sizeDelta = new Vector2(width, (float)(height * 0.68));
+        main.sizeDelta = new Vector2((float)(width * 0.89), (float)(height * 0.68));
+        equip.sizeDelta = new Vector2((float)(width * 0.1), (float)(height * 0.68));
         information.sizeDelta = new Vector2(width, (float)(height * 0.20));
     }
 
@@ -84,7 +87,8 @@ public class PauseScript : MonoBehaviour {
             if (changeWindouSize == true)
             {
                 title.sizeDelta = new Vector2(width, (float)(height * 0.11));
-                main.sizeDelta = new Vector2(width, (float)(height * 0.68));
+                main.sizeDelta = new Vector2((float)(width * 0.89), (float)(height * 0.68));
+                equip.sizeDelta = new Vector2((float)(width * 0.1), (float)(height * 0.68));
                 information.sizeDelta = new Vector2(width, (float)(height * 0.20));
                 changeWindouSize = false;
             }
