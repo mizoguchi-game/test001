c568c28b49bff5761f3efe14bd3f513097bd5e35
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-06-14 00:09:09 +0900

    変数定義までOK

---
+++ RPGtest/Assets/script/Message.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour {
+
+    private Text messageText;//メッセージUI
+    private string message;//表示するメッセージ
+    [SerializeField]
+    private int maxTextLength = 90;//一回のメッセージの最大文字数 
+    private int testLength = 0;//一回のメッセージの現在文字数
+    [SerializeField]
+    private int maxLine = 3;//一回のメッセージの最大行数
+    private int nowLine = 0;//現在の行
+    [SerializeField]
+    private float textSpeed = 0.05f;//テキストスピード
+    private float elapsdTime = 0f;//経過時間
+    private int nowTextNum = 0;//今見ている文字番号
+    private Image clickIcon;
+    [SerializeField]
+    private float ClickFlashTime = 0.2f;//クリックアイコン
+    private bool isOneMessage = false;//一回分のメッセージを表示したかどうか
+    private bool isEndMessage = false;//メッセージすべてを表示したかどうか。
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
