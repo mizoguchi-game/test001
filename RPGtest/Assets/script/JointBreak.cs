53f5c28d1ea91a4cf65de89d7997dfd3d2a79642
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-06-19 00:36:00 +0900

    撃ち落とすところまでは完成

---
+++ RPGtest/Assets/script/JointBreak.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointBreak : MonoBehaviour {
+
+    [SerializeField] private GameObject gameObject;
+
+    public void OnJointBreak(float breakForce)
+    {
+        Destroy(gameObject);
+    }
+}
