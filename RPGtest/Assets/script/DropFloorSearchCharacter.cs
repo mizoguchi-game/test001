9419c5ece59317b1c4d9eefe3ef6e1dfc7641dd0
DESKTOP-REP1T30\yuutaroo01 <mizoguchi.game@gmail.com>
2018-06-21 00:43:40 +0900

    落ちる床作成開始

---
+++ RPGtest/Assets/script/DropFloorSearchCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropFloorSearchCharacter : MonoBehaviour {
+    
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag =="NPC")
+        {
+            GetComponentInParent<FallFloor>().setState(FallFloor.CollisionState.Enter);
+        }        
+    }
+
+    private void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "NPC")
+        {
+            GetComponentInParent<FallFloor>().setState(FallFloor.CollisionState.Stay);
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "NPC")
+        {
+            GetComponentInParent<FallFloor>().setState(FallFloor.CollisionState.Exit);
+        }
+    }
+
+    
+}
---
+++ RPGtest/Assets/script/FallFloor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallFloor : MonoBehaviour {
+
+    public enum CollisionState
+    {
+        Exit,
+        Stay,
+        Enter,
+        
+    }
+
+    private CollisionState state;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void setState(CollisionState dropState)
+    {
+        state = dropState;
+    }
+}
