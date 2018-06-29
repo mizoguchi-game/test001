a116f398726ca141ee10e25c81be67705824f9c2
mizoguchi-game <mizoguchi.game@gmail.com>
2018-06-29 18:13:03 +0900

    カメラ修正

--- RPGtest/Assets/script/CameraController.cs
+++ RPGtest/Assets/script/CameraController.cs
@@ -13,8 +13,8 @@ public class CameraController : MonoBehaviour {
     private bool fpsMode = false;
     private Vector3 tpsPos = new Vector3();
     private float playerMoveAngle = 0f;
-    private float playerAngleY;
-    private float Angular_diff;
+    private float playerAngleY;//プレイヤーの現在の角度
+    private float Angular_diff;//プレイヤーの回転角度差分
     private float minAngleXLimit = -80;
     private float maxAngleXLimit = 80;
     private float minAngleYLimit = -80;
@@ -24,7 +24,7 @@ public class CameraController : MonoBehaviour {
     void Start () {
         targetPos = player.transform.position;
         inFpsPos = transform.position;
-        playerAngleY = player.transform.root.eulerAngles.y;
+        playerAngleY = player.transform.root.gameObject.transform.eulerAngles.y;
     }
 	
 	// Update is called once pedr frame
@@ -34,12 +34,16 @@ public class CameraController : MonoBehaviour {
 
     private void RotateCameraAngele()
     {
-        Angular_diff = (playerAngleY - player.transform.root.eulerAngles.y)*-1;
-        playerAngleY = player.transform.root.eulerAngles.y;
+        
 
         float zoom = Input.GetAxis("Mouse ScrollWheel");
         float angleX = Input.GetAxis("Mouse X");
         float angleY = Input.GetAxis("Mouse Y");
+        float cameraXAngle = 0f;
+        float cameraYAngle = 0f;
+
+        Angular_diff = (playerAngleY - player.transform.root.eulerAngles.y) * -1;
+        playerAngleY = player.transform.root.gameObject.transform.eulerAngles.y;
 
         transform.position += player.transform.position - targetPos;
         targetPos = player.transform.position;
@@ -66,38 +70,33 @@ public class CameraController : MonoBehaviour {
         }
         else
         {
-            /*上下角-+180度 左右角80度*/
+            float rotateYminLimit = (playerAngleY + minAngleYLimit < 0) ? playerAngleY + minAngleYLimit + 360: playerAngleY + minAngleYLimit;
+            float rotateYmaxLimit = (playerAngleY + minAngleYLimit > 360) ? playerAngleY + maxAngleYLimit - 360 : playerAngleY + maxAngleYLimit;
+            Debug.Log("rotateYminLimit:" + rotateYminLimit);
+            Debug.Log("rotateYmaxLimit:" + rotateYmaxLimit);
 
-
-            //transform.Rotate(Vector3.up,angleX * Time.deltaTime + Angular_diff);
-            //transform.Rotate(new Vector3(1f,0f,0f),angleY * Time.deltaTime*-1);
-            //transform.Rotate(new Vector3(0, 0, 1),transform.eulerAngles.z * -1);
-
-            
             // 現在の回転角度を0～360から-180～180に変換
-            float rotateY = (transform.eulerAngles.y > 180) ? transform.eulerAngles.y - 360 : transform.eulerAngles.y;
             float rotateX = (transform.eulerAngles.x > 180) ? transform.eulerAngles.x - 360 : transform.eulerAngles.x;
-            Debug.Log("rotateY:"+rotateY);
+            float rotateY = transform.eulerAngles.y;
 
-            // 現在の回転角度に入力(turn)を加味した回転角度をMathf.Clamp()を使いminAngleからMaxAngle内に収まるようにする
-            float Y = Mathf.Clamp(rotateY + angleX * fpsRotetaspeed, minAngleXLimit, maxAngleXLimit);
-            float X = Mathf.Clamp(rotateX + angleY * fpsRotetaspeed * -1, minAngleYLimit, maxAngleYLimit);
-            Debug.Log("Y1:" + Y);
+            cameraXAngle = Mathf.Clamp(rotateX + angleY * fpsRotetaspeed * -1, minAngleYLimit, maxAngleYLimit);
+            if (rotateYminLimit > rotateYmaxLimit)
+            {
+                Debug.Log("min大きい");
+                cameraYAngle = Mathf.Clamp(rotateY + angleX * fpsRotetaspeed + Angular_diff, rotateYmaxLimit, rotateYminLimit);
+            }
+            else
+            {
+                Debug.Log("max大きい");
+                cameraYAngle = Mathf.Clamp(rotateY + angleX * fpsRotetaspeed + Angular_diff, rotateYminLimit, rotateYmaxLimit);
+            }
+            Debug.Log("cameraYAngle:" + cameraYAngle);
+
             // 回転角度を-180～180から0～360に変換
-            Y = (Y < 0) ? Y + 360 : Y;
-            X = (X < 0) ? X + 360 : X;
-            Debug.Log("Y2:" + Y);
-            // 回転角度をオブジェクトに適用
-            transform.rotation = Quaternion.Euler(X, Y + Angular_diff, 0);
-            
+            cameraXAngle = (cameraXAngle < 0) ? cameraXAngle + 360 : cameraXAngle;
 
-            /*
-            // 現在の回転角度に入力(turn)を加味した回転角度をMathf.Clamp()を使いminAngleからMaxAngle内に収まるようにする
-            float Y = Mathf.Clamp(transform.eulerAngles.y + angleX * fpsRotetaspeed, minAngleXLimit, maxAngleXLimit);
-            //float X = Mathf.Clamp(transform.eulerAngles.x + angleY * fpsRotetaspeed * -1, minAngleYLimit + player.transform.root.eulerAngles.x, maxAngleYLimit + player.transform.root.eulerAngles.x);
             // 回転角度をオブジェクトに適用
-            transform.rotation = Quaternion.Euler(0, Y + Angular_diff, 0);
-            */
+            transform.rotation = Quaternion.Euler(/*cameraXAngle*/0,cameraYAngle, 0);            
         }
     }
 }
